5bd5e65adfd5decf305c69075f0f76e522ee7fd9
HW-Faction <hankwilliamson49@gmail.com>
2019-07-17 11:32:24 +0530

    Updates

---
+++ precache-manifest.ca8b310d852d4243f47dcfd2458c5b07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86921d363fe16efdb52a074513ac610",
+    "url": "/RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "97777d843b9996f2ee4d",
+    "url": "/RandomQuoteMachine/static/css/main.0c038d0e.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/RandomQuoteMachine/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "97777d843b9996f2ee4d",
+    "url": "/RandomQuoteMachine/static/js/main.d2704e79.chunk.js"
+  },
+  {
+    "revision": "b3a13b304a01825b085f",
+    "url": "/RandomQuoteMachine/static/js/runtime~main.fbe941b2.js"
+  }
+]);
